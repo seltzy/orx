7f2af34c4723d885f20c92c3290a72cbad39e677
Sam Seltzer-Johnston <sam@sbseltzer.net>
2017-03-15 15:35:12 -0400

    Sample usage.

--- tutorial/src/13_Text.c
+++ tutorial/src/13_Text.c
@@ -49,6 +49,167 @@
  * then relaunch this tutorial. For an exhaustive list of options, please look at CreationTemplate.ini.
  */
 
+orxTEXT *pstTestText = orxNULL;
+
+static void TestMarkerTraversal(orxTEXT *_pstText, orxBITMAP *pstBitmap, orxCHARACTER_MAP *pstMap)
+{
+  const orxSTRING zString = orxText_GetString(_pstText);
+  orxLINKLIST stStack = {0};
+  orxHANDLE hMarker = orxNULL;
+  orxHANDLE hFallback = orxNULL;
+  orxHANDLE hIterator = orxText_GetMarkerIterator(_pstText);
+  orxLOG("Testing markers for %s", zString);
+  for (orxU32 u32Index = 0; u32Index < orxString_GetLength(zString); u32Index++)
+  {
+    for (;
+         (hIterator != orxNULL) && (hIterator != orxHANDLE_UNDEFINED) && (orxText_GetMarkerIndex(hIterator) == u32Index) ;
+         hIterator = orxText_NextMarker(hIterator))
+    {
+      orxTEXT_MARKER_TYPE eType = orxText_GetMarkerType(hIterator);
+      hFallback = orxNULL;
+      switch(eType)
+      {
+      case orxTEXT_MARKER_TYPE_NONE:
+      {
+        orxLOG("No marker @%u?", u32Index);
+        break;
+      }
+      case orxTEXT_MARKER_TYPE_COLOR:
+      {
+        orxRGBA stColor = {0};
+        if (orxText_GetMarkerColor(hIterator, &stColor) == orxSTATUS_SUCCESS)
+        {
+          orxLOG("Hit %s Marker @%u (%u, %u, %u, %u)", "color", u32Index, stColor.u8R, stColor.u8G, stColor.u8B, stColor.u8A);
+          orxLinkList_AddEnd(&stStack, hIterator);
+        }
+        break;
+      }
+      case orxTEXT_MARKER_TYPE_FONT:
+      {
+        const orxFONT *pstFont;
+        if (orxText_GetMarkerFont(hIterator, &pstFont) == orxSTATUS_SUCCESS)
+        {
+          orxLOG("Hit %s Marker @%u %s", "font", u32Index, orxFont_GetName(pstFont));
+          orxLinkList_AddEnd(&stStack, hIterator);
+        }
+        break;
+      }
+      case orxTEXT_MARKER_TYPE_SCALE:
+      {
+        orxVECTOR vScale = {0};
+        if (orxText_GetMarkerScale(hIterator, &vScale) == orxSTATUS_SUCCESS)
+        {
+          orxLOG("Hit %s Marker @%u (%d, %d, %d)", "scale", u32Index, vScale.fX, vScale.fY, vScale.fZ);
+          orxLinkList_AddEnd(&stStack, hIterator);
+        }
+        break;
+      }
+      case orxTEXT_MARKER_TYPE_POP:
+        hFallback = orxText_GetMarkerFallback(orxLinkList_GetLast(&stStack));
+        if (orxLinkList_GetCounter(&stStack) > 0)
+        {
+          orxLinkList_Remove(orxLinkList_GetLast(&stStack));
+        }
+        break;
+      case orxTEXT_MARKER_TYPE_CLEAR:
+        hFallback = orxNULL;
+        orxLinkList_Clean(&stStack);
+        break;
+      default:
+        orxLOG("Unknown marker @%u?", u32Index);
+      }
+      hMarker = orxLinkList_GetLast(&stStack);
+      if ((hMarker != orxNULL) && (hMarker != orxHANDLE_UNDEFINED))
+      {
+        /* orxLOG("Marker is in use"); */
+        orxU32 u32LastMarkerIndex = orxText_GetMarkerIndex(hMarker);
+        eType = orxText_GetMarkerType(hMarker);
+        switch(eType)
+        {
+        case orxTEXT_MARKER_TYPE_COLOR:
+        {
+          orxRGBA stColor = {0};
+          if (orxText_GetMarkerColor(hMarker, &stColor) == orxSTATUS_SUCCESS)
+          {
+            orxLOG("Using %s Marker @%u (%u, %u, %u, %u)", "color", u32LastMarkerIndex, stColor.u8R, stColor.u8G, stColor.u8B, stColor.u8A);
+          }
+          break;
+        }
+        case orxTEXT_MARKER_TYPE_FONT:
+        {
+          const orxFONT *pstFont;
+          if (orxText_GetMarkerFont(hMarker, &pstFont) == orxSTATUS_SUCCESS)
+          {
+            orxLOG("Using %s Marker @%u %s", "font", u32LastMarkerIndex, orxFont_GetName(pstFont));
+          }
+          break;
+        }
+        case orxTEXT_MARKER_TYPE_SCALE:
+        {
+          orxVECTOR vScale = {0};
+          if (orxText_GetMarkerScale(hMarker, &vScale) == orxSTATUS_SUCCESS)
+          {
+            orxLOG("Using %s Marker @%u (%d, %d, %d)", "scale", u32LastMarkerIndex, vScale.fX, vScale.fY, vScale.fZ);
+          }
+          break;
+        }
+        default:
+          orxLOG("Invalid marker type @%u", u32LastMarkerIndex);
+        }
+        if ((hFallback != orxNULL) && (hFallback != orxHANDLE_UNDEFINED))
+        {
+          /* orxLOG("Marker fallback exists - applying changes..."); */
+          orxU32 u32LastMarkerIndex = orxText_GetMarkerIndex(hFallback);
+          eType = orxText_GetMarkerType(hFallback);
+          switch(eType)
+          {
+          case orxTEXT_MARKER_TYPE_COLOR:
+          {
+            orxRGBA stColor = {0};
+            if (orxText_GetMarkerColor(hFallback, &stColor) == orxSTATUS_SUCCESS)
+            {
+              orxLOG("Apply fallback %s Marker @%u (%u, %u, %u, %u)", "color", u32LastMarkerIndex, stColor.u8R, stColor.u8G, stColor.u8B, stColor.u8A);
+            }
+            break;
+          }
+          case orxTEXT_MARKER_TYPE_FONT:
+          {
+            const orxFONT *pstFont = orxNULL;
+            if (orxText_GetMarkerFont(hFallback, &pstFont) == orxSTATUS_SUCCESS)
+            {
+              orxLOG("Apply fallback %s Marker @%u %s", "font", u32LastMarkerIndex, orxFont_GetName(pstFont));
+            }
+            break;
+          }
+          case orxTEXT_MARKER_TYPE_SCALE:
+          {
+            orxVECTOR vScale = {0};
+            if (orxText_GetMarkerScale(hFallback, &vScale) == orxSTATUS_SUCCESS)
+            {
+              orxLOG("Apply fallback %s Marker @%u (%d, %d, %d)", "scale", u32LastMarkerIndex, vScale.fX, vScale.fY, vScale.fZ);
+            }
+            break;
+          }
+          default:
+            orxLOG("Invalid marker type @%u", u32LastMarkerIndex);
+          }
+        }
+      }
+    }
+  }
+  orxLinkList_Clean(&stStack);
+}
+
+orxSTATUS orxFASTCALL ConfigEventHandler(const orxEVENT *_pstEvent) {
+  orxSTATUS eResult = orxSTATUS_SUCCESS;
+  if (_pstEvent->eID == orxRESOURCE_EVENT_UPDATE) {
+    orxConfig_PushSection(orxText_GetName(pstTestText));
+    orxText_SetString(pstTestText, orxConfig_GetString("String"));
+    orxConfig_PopSection();
+    TestMarkerTraversal(pstTestText, orxNULL, orxNULL);
+  }
+  return eResult;
+}
 
 /** Inits the tutorial
  */
@@ -59,12 +220,18 @@ orxSTATUS orxFASTCALL Init()
          "\n* You can play with the config parameters in ../13_Text.ini"
          "\n* After changing them, relaunch the tutorial to see their effects");
 
+  orxEvent_AddHandler(orxEVENT_TYPE_RESOURCE, ConfigEventHandler);
   /* Creates viewport */
   orxViewport_CreateFromConfig("Viewport");
 
   /* Creates object */
   orxObject_CreateFromConfig("Scene");
 
+  orxOBJECT *pstMyTextObject = orxObject_CreateFromConfig("TextObject");
+  orxGRAPHIC *pstGraphic = orxGRAPHIC(orxOBJECT_GET_STRUCTURE( pstMyTextObject, GRAPHIC) ) ;
+	orxSTRUCTURE *pstStructure = orxGraphic_GetData( pstGraphic );
+  pstTestText = orxTEXT(pstStructure);
+  TestMarkerTraversal(pstTestText, orxNULL, orxNULL);
   /* Done! */
   return orxSTATUS_SUCCESS;
 }
@@ -80,6 +247,7 @@ orxSTATUS orxFASTCALL Run()
   {
     /* Updates result */
     eResult = orxSTATUS_FAILURE;
+    orxLOG("TEST");
   }
 
   /* Done! */
